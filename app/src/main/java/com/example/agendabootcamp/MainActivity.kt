ced8cb8edb36d53f35141f95a545429123f601d4
allan8araujo@gmail.com <allan8araujo>
2022-01-24 22:02:56 -0300

    Todo List App > First project

---
+++ app/src/main/java/com/example/agendabootcamp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.agendabootcamp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_todo.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var todoAdapter: TodoAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        todoAdapter= TodoAdapter(mutableListOf())
+
+        rvTodoItems.adapter=todoAdapter
+        rvTodoItems.layoutManager=LinearLayoutManager(this)
+
+        btnAddTodoList.setOnClickListener {
+            val todoTitle=etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()){
+                val todo= Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+
+        btnDeleteTodoList.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+
+        }
+    }
---
+++ app/src/main/java/com/example/agendabootcamp/Todo.kt
@@ -0,0 +1,6 @@
+package com.example.agendabootcamp
+
+data class Todo (
+    val title:String,
+    var isChecked:Boolean=false
+)
