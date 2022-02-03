73db17d0130201a742ca3b9e381994373db1087d
allan8araujo@gmail.com <allan8araujo>
2022-02-03 11:37:02 -0300

    adicionado toast msg

--- app/src/main/java/com/example/catapp/MainActivity.kt
+++ app/src/main/java/com/example/catapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.catapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+    fun onTap(view: View) {
+        val btnChecarGato= findViewById(R.id.btnCatSearch) as Button
+        btnChecarGato.setOnClickListener {
+            Toast.makeText(this, "Eu acho que eu vi um gatinho", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
