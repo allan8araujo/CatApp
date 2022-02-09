ec71b27441234e2fb7225c3e3e6bea04bc890b6a
allan8araujo@gmail.com <allan8araujo>
2022-02-09 13:11:10 -0300

    catApp v1.0 Agora acompanha uma barra de loading adicionada pela uiThread

--- app/src/main/java/com/example/catapp/HttpHelper.kt
+++ app/src/main/java/com/example/catapp/HttpHelper.kt
@@ -9,9 +9,9 @@ class HttpHelper //classe responsável pela funget
     fun get(URL:String):Bitmap? {
         val client=OkHttpClient()//criando cliente que vai disparar a requisição
         val request=Request.Builder().url(URL).build() //criando a requisição GET na URL
-        val response=client.newCall(request).execute() // pegando o componente da okHttpClient e
-        val inputStream =response.body?.byteStream()
-        var bitmaping=BitmapFactory.decodeStream(inputStream)
-        return bitmaping //efetivamente retorna o bitmap
+        val response=client.newCall(request).execute() // pegando resposta do servidor na chamada newCall(request)
+        val inputStream =response.body?.byteStream() //pegando o body da resposta em bytestream
+        var bitmaping=BitmapFactory.decodeStream(inputStream) //decodando a stream do inputStream
+        return bitmaping //efetivamente retorna o bitmap derivado do request
     }
 }
--- app/src/main/java/com/example/catapp/MainActivity.kt
+++ app/src/main/java/com/example/catapp/MainActivity.kt
@@ -2,21 +2,37 @@ package com.example.catapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.ProgressBar
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnMostrarGato= findViewById<Button>(R.id.btnCatSearch)
-        btnMostrarGato.setOnClickListener {
+        var progressBar=findViewById<ProgressBar>(R.id.pbLoading)
+
+
+        btnMostrarGato.setOnClickListener { //seta o evento btnCLick
+            progressBar.visibility=View.VISIBLE
+
             doAsync {
-                val imgCat=findViewById<ImageView>(R.id.imgCat)
-                val newimg=HttpHelper().get("https://cataas.com/c")
-                imgCat.setImageBitmap(newimg)  }
+                val imgCat=findViewById<ImageView>(R.id.imgCat) //vai pegar a imagem imgCat
+                val newimg=HttpHelper().get("https://cataas.com/c") //joga o link da api e chama o método
+
+                uiThread {
+                    progressBar.visibility=View.INVISIBLE
+                    imgCat.setImageBitmap(newimg)
+                    }
+                }
         }
 
     }
