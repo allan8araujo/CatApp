56c09ad85f09a75dc03c8af55284eb02442ed4a8
allan8araujo@gmail.com <allan8araujo>
2022-04-28 16:47:15 -0300

    MVP CatApp

--- app/src/main/java/com/example/catapp/HttpHelper.kt
+++ app/src/main/java/com/example/catapp/HttpHelper.kt
@@ -7,7 +7,7 @@ import okhttp3.*
 class HttpHelper //classe responsável pela funget
 {
     fun get(URL: String): Bitmap? {
-        val client = OkHttpClient()//criando cliente que vai disparar a requisição
+        val client = OkHttpClient()//criando clienter que vai disparar a requisição
         val request = Request.Builder().url(URL).build() //criando a requisição GET na URL
         val response = client.newCall(request)
             .execute() // pegando resposta do servidor na chamada newCall(request)
