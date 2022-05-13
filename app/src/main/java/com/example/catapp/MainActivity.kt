547ccb09bc239df6396bc51ae447b715a205174d
allan8araujo@gmail.com <allan8araujo>
2022-05-13 14:06:02 -0300

    Some good practices changes. Also the Ktlint plugin is now the pattern for good practices. Some bug fix as well.

--- app/src/main/java/com/example/catapp/MainActivity.kt
+++ app/src/main/java/com/example/catapp/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.catapp
 
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.catapp.databinding.ActivityMainBinding
@@ -20,29 +20,27 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         supportActionBar?.hide()
 
         biding.btnCatSearch.setOnClickListener(this)
-
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.btnCatSearch) {
             val progressBar = biding.pbLoading
 
-            progressBar.visibility=View.VISIBLE
+            progressBar.visibility = View.VISIBLE
             val repository = Repository()
             val viewModelFactory = MainViewModelFactory(repository)
             viewModel =
                 ViewModelProvider(this, viewModelFactory)
-                .get(MainViewModel::class.java)
+                    .get(MainViewModel::class.java)
             viewModel.getImage()
 
             Glide.with(biding.root.context)
                 .load(BitmapFactory.decodeStream(viewModel.myResponse.value?.byteStream()))
-                .placeholder(R.drawable.pb_loading)
+//                .placeholder(R.drawable.pb_loading)
                 .centerCrop()
                 .into(biding.imgCat)
 
-            progressBar.visibility=View.GONE
-
+            progressBar.visibility = View.GONE
         }
     }
 }
--- app/src/main/java/com/example/catapp/api/CataasApi.kt
+++ app/src/main/java/com/example/catapp/api/CataasApi.kt
@@ -3,8 +3,7 @@ package com.example.catapp.api
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 
-interface catApi{
+interface catApi {
     @GET("c")
     suspend fun getImage(): ResponseBody
-
 }
