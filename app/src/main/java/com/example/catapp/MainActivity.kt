f5d9d9569f1d29395c9456dc725071fdbab31867
allan8araujo <allanzao08@gmail.com>
2022-05-16 23:52:42 -0300

    Navigation still being implemented. Also some improvement to layout.

--- app/src/main/java/com/example/catapp/MainActivity.kt
+++ app/src/main/java/com/example/catapp/MainActivity.kt
@@ -2,9 +2,10 @@ package com.example.catapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.example.catapp.databinding.ActivityMainBinding
-import com.example.catapp.ui.fragments.HistoryFragment
+import com.example.catapp.ui.fragments.CatFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +16,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(biding.root)
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, HistoryFragment())
-        transaction.commit()
+
+        val transactionNavigation = supportFragmentManager.beginTransaction()
+        transactionNavigation.replace(R.id.fragment_container, CatFragment())
+        transactionNavigation.commit()
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        if (fragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
