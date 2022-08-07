854c9e265e1a480365832443e7e0d80557d3d4b3
allan8araujo <allanzao08@gmail.com>
2022-08-07 12:46:03 -0300

    [FIX] fix crash issue

--- database/src/main/java/com/example/database/daos/CatDao.kt
+++ database/src/main/java/com/example/database/daos/CatDao.kt
@@ -7,16 +7,16 @@ import androidx.room.Query
 import com.example.abstractions.CatPhoto
 
 @Dao
-interface CatDao {
+abstract class CatDao {
     @Query("SELECT * FROM cat_photos ORDER BY id DESC LIMIT :limit OFFSET :offset")
-    suspend fun getCatsFromDB(limit: Int, offset: Int): List<CatPhoto>
+    abstract suspend fun getCatsFromDB(limit: Int, offset: Int): List<CatPhoto>
 
     @Insert
-    suspend fun insertCatInDB(cat: CatPhoto?)
+    abstract suspend fun insertCatInDB(cat: CatPhoto?)
 
     @Delete
-    suspend fun deleteCatInDB(cat: CatPhoto?)
+    abstract suspend fun deleteCatInDB(cat: CatPhoto?)
 
     @Query("DELETE FROM cat_photos")
-    suspend fun deleteAllInDB(): Int
+    abstract fun deleteAllInDB()
 }
