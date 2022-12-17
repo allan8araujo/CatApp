6b8efdbdf1654eb6d0531a84950034641fddaadc
allan <allan8araujo@gmail.com>
2022-12-16 21:45:05 -0300

    feature gif implanted

--- app/src/main/java/com/example/catapp/presenter/util/MainViewModelFactory.kt
+++ app/src/main/java/com/example/catapp/presenter/util/MainViewModelFactory.kt
@@ -3,10 +3,17 @@ package com.example.catapp.presenter.util
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.catapp.data.Repository
+import com.example.catapp.presenter.viewModel.GifScreenViewModel
 import com.example.catapp.presenter.viewModel.MainScreenViewModel
 
 class MainViewModelFactory(val repository: Repository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return MainScreenViewModel(repository) as T
+    }
+}
+
+class GifViewModelFactory(val repository: Repository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return GifScreenViewModel(repository) as T
     }
 }
--- app/src/main/java/com/example/catapp/presenter/view/fragments/GifScreenFragment.kt
+++ app/src/main/java/com/example/catapp/presenter/view/fragments/GifScreenFragment.kt
@@ -1,28 +1,48 @@
 package com.example.catapp.presenter.view.fragments
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import com.example.catapp.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.example.catapp.data.Repository
+import com.example.catapp.databinding.FragmentGifScreenBinding
+import com.example.catapp.presenter.util.GifViewModelFactory
 import com.example.catapp.presenter.viewModel.GifScreenViewModel
-import com.example.catapp.presenter.viewModel.MainScreenViewModel
+import com.example.database.CatDataBaseInstance
 
 class GifScreenFragment : Fragment() {
-    private val gifScreenViewModel: GifScreenViewModel by activityViewModels()
+
+    private val database by lazy { CatDataBaseInstance.CatRoomDatabase.getDataBase(requireContext()) }
+    private val gifScreenViewModel: GifScreenViewModel by activityViewModels {
+        GifViewModelFactory(Repository(database.appDao()))
+    }
+    lateinit var binding: FragmentGifScreenBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
+        binding = FragmentGifScreenBinding.inflate(inflater, container, false)
+        val view = binding.root
         gifScreenViewModel.getGif()
-        gifScreenViewModel.catResponseGif.observe(viewLifecycleOwner){
 
+
+        gifScreenViewModel.catResponseGif.observe(viewLifecycleOwner) { responseBody ->
+            val responseByteArray = responseBody?.byteStream()?.readBytes()
+
+            Glide.with(binding.root.context)
+                .load(responseByteArray)
+                .centerCrop()
+//                .listener(ProgressBarListener(this))
+                .into(binding.gifCat)
         }
-        return inflater.inflate(R.layout.fragment_gif_screen, container, false)
+
+        return view
     }
 }
